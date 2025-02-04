f2395cc0e254cb15e567fd6896bcab122c99679e
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2025-02-04 23:58:02 +0530

    Update tree3.cpp

--- tree3.cpp
+++ tree3.cpp
@@ -16,6 +16,7 @@ class BT{
         data=x;
         left=NULL;
         right=NULL;
+        cn=0;
     }
     void insert(BT *current,BT *temp)
     {
@@ -83,29 +84,37 @@ class BT{
         }
     }
 
-    void count(BT *index,int cnt)
+    void count(BT *index)
     {
         if(index==NULL)
         {
            return;
         }else{
-            cnt++;
-            count(index->left,cnt);
-            count(index->right,cnt);
+            cn++;
+            cout<<"cn="<<cn<<endl;
+            count(index->left);
+            count(index->right);
             
         }
-        cn=cnt;
+        
+        
     }
-    void noleaf(BT *index,int cntl)
+    void noleaf(BT *index)
     {
         if(index->left==NULL && index->right==NULL)
         {
-            cntl++;
-        }else{
-            noleaf(index->left,cntl);
-            noleaf(index->right,cntl);
+            cl++;
+            cout<<"cl="<<cl<<endl;
+            return;
+        }else if(index->left!=NULL)
+        {
+            noleaf(index->left);
+        }else if(index->right!=NULL)
+        {
+            noleaf(index->right);
         }
-        cl=cntl;
+        
+        
     }
 };
 int main()
@@ -122,7 +131,7 @@ int main()
                    cout<<"Enter data:";
                    cin>>s;
                    root=new BT(s);
-                   b.cn=1;
+                   b.cn=0;
                    b.cl=0;
                    break;
             case 2://insert
@@ -141,14 +150,344 @@ int main()
                    b.postorder(root);
                    break;
             case 6://node count
-                //    b.cn=0;
+                   b.cn=0;
                 
-                   b.count(root,b.cn);
+                   b.count(root);
                    cout<<"\nNo.of nodes : "<<b.cn<<endl;
                    break;
             case 7://no.of leaf node
-                //    b.cl=0;
-                   b.noleaf(root,b.cl);
+                   b.cl=0;
+                   b.noleaf(root);
+                   cout<<"\nNo.of leaf nodes : "<<b.cl<<endl;
+           
+                   
+        }
+    }while(ch!=8);
+}//binary tree
+#include<iostream>
+using namespace std;
+class BT{
+    public:
+    int data;
+    BT *left;
+    BT *right;
+    int cn,cl;
+    BT()
+    {
+
+    }
+    BT(int x)
+    {
+        data=x;
+        left=NULL;
+        right=NULL;
+        cn=0;
+    }
+    void insert(BT *current,BT *temp)
+    {
+        int s;
+        char ch1;
+        if(current==NULL)
+        {
+            current=temp;
+        }else{
+            cout<<"enter left(l)/right(r):";
+            cin>>ch1;
+            if(ch1=='l' || ch1=='L')
+            {
+                if(current->left==NULL)
+                {
+                    current->left=temp;
+                }else{
+                    cout<<"left is NOT NULL\n";
+                    insert(current->left,temp);
+
+                }
+            }else{
+                if(current->right==NULL)
+                {
+                    current->right=temp;
+                }else{
+                    cout<<"right is NOT NULL\n";
+                    insert(current->right,temp);
+
+                }
+            }
+        }
+    }
+    void preorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            cout<<index->data<<"\t";
+            preorder(index->left);
+            preorder(index->right);
+        }
+    }
+    void inorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            inorder(index->left);
+            cout<<index->data<<"\t";
+            inorder(index->right);
+        }
+    }
+    void postorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            postorder(index->left);
+            postorder(index->right);
+            cout<<index->data<<"\t";
+        }
+    }
+
+    void count(BT *index)
+    {
+        if(index==NULL)
+        {
+           return;
+        }else{
+            cn++;
+            cout<<"cn="<<cn<<endl;
+            count(index->left);
+            count(index->right);
+            
+        }
+        
+        
+    }
+    void noleaf(BT *index)
+    {
+        if(index->left==NULL && index->right==NULL)
+        {
+            cl++;
+            cout<<"cl="<<cl<<endl;
+            return;
+        }else if(index->left!=NULL)
+        {
+            noleaf(index->left);
+        }else if(index->right!=NULL)
+        {
+            noleaf(index->right);
+        }
+        
+        
+    }
+};
+int main()
+{
+    int ch,s,no;
+    BT *root,*newnode;
+    BT b;
+    do{
+        cout<<"\n1.Create\t2.insert\t3.Preorder\t4.Inorder\t5.Postorder\t6.No.of nodes\t7.No.of leaf nodes\t8.Exit\nEnter choice:";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1://create
+                   cout<<"Enter data:";
+                   cin>>s;
+                   root=new BT(s);
+                   b.cn=0;
+                   b.cl=0;
+                   break;
+            case 2://insert
+                   cout<<"Enter data:";
+                   cin>>s;
+                   newnode=new BT(s);
+                   b.insert(root,newnode);
+                   break;
+            case 3://preorder display
+                    b.preorder(root);
+                    break;
+            case 4://inorder
+                    b.inorder(root);
+                    break;
+            case 5://postorder
+                   b.postorder(root);
+                   break;
+            case 6://node count
+                   b.cn=0;
+                
+                   b.count(root);
+                   cout<<"\nNo.of nodes : "<<b.cn<<endl;
+                   break;
+            case 7://no.of leaf node
+                   b.cl=0;
+                   b.noleaf(root);
+                   cout<<"\nNo.of leaf nodes : "<<b.cl<<endl;
+           
+                   
+        }
+    }while(ch!=8);
+}//binary tree
+#include<iostream>
+using namespace std;
+class BT{
+    public:
+    int data;
+    BT *left;
+    BT *right;
+    int cn,cl;
+    BT()
+    {
+
+    }
+    BT(int x)
+    {
+        data=x;
+        left=NULL;
+        right=NULL;
+        cn=0;
+    }
+    void insert(BT *current,BT *temp)
+    {
+        int s;
+        char ch1;
+        if(current==NULL)
+        {
+            current=temp;
+        }else{
+            cout<<"enter left(l)/right(r):";
+            cin>>ch1;
+            if(ch1=='l' || ch1=='L')
+            {
+                if(current->left==NULL)
+                {
+                    current->left=temp;
+                }else{
+                    cout<<"left is NOT NULL\n";
+                    insert(current->left,temp);
+
+                }
+            }else{
+                if(current->right==NULL)
+                {
+                    current->right=temp;
+                }else{
+                    cout<<"right is NOT NULL\n";
+                    insert(current->right,temp);
+
+                }
+            }
+        }
+    }
+    void preorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            cout<<index->data<<"\t";
+            preorder(index->left);
+            preorder(index->right);
+        }
+    }
+    void inorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            inorder(index->left);
+            cout<<index->data<<"\t";
+            inorder(index->right);
+        }
+    }
+    void postorder(BT *index)
+    {
+        if(index==NULL)
+        {
+            return;
+        }else{
+            postorder(index->left);
+            postorder(index->right);
+            cout<<index->data<<"\t";
+        }
+    }
+
+    void count(BT *index)
+    {
+        if(index==NULL)
+        {
+           return;
+        }else{
+            cn++;
+            cout<<"cn="<<cn<<endl;
+            count(index->left);
+            count(index->right);
+            
+        }
+        
+        
+    }
+    void noleaf(BT *index)
+    {
+        if(index->left==NULL && index->right==NULL)
+        {
+            cl++;
+            cout<<"cl="<<cl<<endl;
+            return;
+        }else if(index->left!=NULL)
+        {
+            noleaf(index->left);
+        }else if(index->right!=NULL)
+        {
+            noleaf(index->right);
+        }
+        
+        
+    }
+};
+int main()
+{
+    int ch,s,no;
+    BT *root,*newnode;
+    BT b;
+    do{
+        cout<<"\n1.Create\t2.insert\t3.Preorder\t4.Inorder\t5.Postorder\t6.No.of nodes\t7.No.of leaf nodes\t8.Exit\nEnter choice:";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1://create
+                   cout<<"Enter data:";
+                   cin>>s;
+                   root=new BT(s);
+                   b.cn=0;
+                   b.cl=0;
+                   break;
+            case 2://insert
+                   cout<<"Enter data:";
+                   cin>>s;
+                   newnode=new BT(s);
+                   b.insert(root,newnode);
+                   break;
+            case 3://preorder display
+                    b.preorder(root);
+                    break;
+            case 4://inorder
+                    b.inorder(root);
+                    break;
+            case 5://postorder
+                   b.postorder(root);
+                   break;
+            case 6://node count
+                   b.cn=0;
+                
+                   b.count(root);
+                   cout<<"\nNo.of nodes : "<<b.cn<<endl;
+                   break;
+            case 7://no.of leaf node
+                   b.cl=0;
+                   b.noleaf(root);
                    cout<<"\nNo.of leaf nodes : "<<b.cl<<endl;
            
                    
