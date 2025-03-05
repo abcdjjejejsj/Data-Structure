5c582d6582872a7a9904ea05ffaac35510ae6356
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2025-03-05 16:59:07 +0530

    Update Assignment_3.cpp

--- Assignment_3.cpp
+++ Assignment_3.cpp
@@ -1,82 +1,139 @@
-#include <iostream>
-#include <string>
-
+//assignment-3 : Graph
+#include<iostream>
 using namespace std;
+class Graph{
+   public:
+    int v,e;
+    int m[100][100];
+    Graph()
+    {
 
-int main() {
-    int n, e; // Number of cities (vertices) and flight paths (edges)
-    
-    cout << "Enter number of cities (vertices): ";
-    cin >> n;
-    cout << "Enter number of flight paths (edges): ";
-    cin >> e;
-
-    // Adjacency matrix initialized to 0
-    int adjMatrix[n][n]; 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            adjMatrix[i][j] = 0;
-        }
     }
-
-    // Adjacency list representation using a 2D array
-    int adjList[n][n]; // This will store destinations, -1 means empty
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            adjList[i][j] = -1; // Initialize with -1 (indicating no connection)
-        }
-    }
-
-    cout << "Enter source city, destination city, and cost (time/fuel) for each flight:\n";
-    for (int i = 0; i < e; i++) {
-        int src, dest, cost;
-        cin >> src >> dest >> cost;
-
-        // Filling the adjacency matrix with cost
-        adjMatrix[src][dest] = cost;
-        adjMatrix[dest][src] = cost; // Since it's an undirected graph
-
-        // Filling adjacency list
-        for (int j = 0; j < n; j++) {
-            if (adjList[src][j] == -1) {
-                adjList[src][j] = dest;
-                break;
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (adjList[dest][j] == -1) {
-                adjList[dest][j] = src;
-                break;
+    Graph(int x,int y)
+    {
+        v=x;
+        e=y;
+        for(int i=1;i<=100;i++)
+        {
+            for(int j=1;j<=100;j++)
+            {
+                m[i][j]=0;
             }
         }
     }
-
-    // Display the Adjacency Matrix
-    cout << "\nAdjacency Matrix Representation:\n  ";
-    for (int i = 0; i < n; i++) {
-        cout << i << " ";
+    void edge(int u,int v)
+    {
+        m[u][v]=1;
+        m[v][u]=1;
     }
-    cout << endl;
-    
-    for (int i = 0; i < n; i++) {
-        cout << i << " ";
-        for (int j = 0; j < n; j++) {
-            cout << adjMatrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+};
+Class list{
+    public:
+    int data;
+    list *next;
 
-    // Display the Adjacency List
-    cout << "\nAdjacency List Representation:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "City " << i << " -> ";
-        for (int j = 0; j < n; j++) {
-            if (adjList[i][j] != -1) {
-                cout << "City " << adjList[i][j] << " ";
+    list(int x)
+    {
+        data=x;
+        next=NULL
+    }
+    list(){
+
+    }
+    void insert(list *hed,list *temp)
+    {
+        list *temp;
+        if(hed==NULL)
+        {
+            hed=temp;
+        }else{
+            while(hed->next==NULL)
+            {
+                hed = hed->next;
             }
+            hed=temp;
         }
-        cout << endl;
     }
+}
+int main()
+{
+    Graph g;
+    Graph *c;
+    list *head[20],*temp;
+    list l;
+    int ch,vertex,edges,v1,v2,i,j,cnt=0,no,d,n;
+    do{
+        cout<<"\n1.Input\t2.Output\t3.Exit\nEnter choice :";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1://input :-adjacency matrix
+                   cout<<"Enter no.of vertices :";
+                   cin>>vertex;
+                   cout<<"Enter no.of edges :";
+                   cin>>edges;
+                   c = new Graph(vertex,edges);
+                   cout<<"Enter vertex that have edges between them :"<<endl;
+                   for(i=0;i<edges;i++)
+                   {
+                    cin>>v1;
+                    cin>>v2;
+                    g.edge(v1,v2);
+                   }
+                   break;
+                
+            case 2://output :adjacency matrix
+                  cout<<"\t";
+                  for(i=1;i<=vertex;i++)
+                  {
+                    cout<<i<<"\t";
+                  }
+                  cout<<endl;
+                  for(i=1;i<=vertex;i++)
+                  {
+                    cout<<i<<"\t";
+                    for(j=1;j<=vertex;j++)
+                    {
+                        cout<<g.m[i][j]<<"\t";
+                    }
+                    cout<<endl;
+                  }
+                  break;
+            case 3://adjacency list
+                   cout<<"Enter no.of vertices :";
+                   cin>>vertex;
+                   for(i=0;i<vertex;i++)
+                   {
+                    hed[cnt]=NULL;
+                    cout<<"Enter no.of adjacent vertices :";
+                    cin>>no;
+                    for(j=0;j<no;j++)
+                    {
+                        cout<<"vertex-"<<j+1<<" :";
+                        cin>>d;
+                        temp=new list(d);
+                        l.insert(hed[cnt],temp);
+                    }
+                    cnt++;
+                   }
 
-    return 0;
+                 break;
+                   
+            case 4://display
+                   n=0;
+                   for(i=0;i<cnt;i++)
+                   {
+                    temp=hed[i];
+
+                    while(hed!=NULL)
+                    {
+
+                        cout<<temp->data<<"\t";
+                    }
+                   }
+                   break;
+
+        }
+    }while(ch!=5);
+
 }
